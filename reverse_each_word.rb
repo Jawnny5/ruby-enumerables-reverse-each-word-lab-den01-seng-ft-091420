25676e2e00dddf9f6e23889d8dcbc13eb56a674f
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-10 15:06:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,11 +9,3 @@ def reverse_each_word_with_each(sentence)
   reversed_array.join(" ")
 end
 
-def reverse_each_word(sentence)
-  reversal = string.split(" ")
-  reversed_array = []
-  reversal.collect do |element|
-    reversed_array << element.reverse
-  end
-  reversed_array.join(" ")
-end
